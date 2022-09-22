9674edcfdaecf42ad371550caef2626976d6548d
Ken Ramos <vonnduven@gmail.com>
2022-09-22 02:21:38 -0700

    pulling data from json, reducing to group by class, simple display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({});
+
+  const keyByClassReducer = (acc, drData) => {
+    const key = drData.class;
+    const val = acc[key] ? [...acc[key], drData] : [drData];
+    acc[key] = val;
+
+    return acc;
+  };
+
+  const fetchJsonDataAndReduce = () => {
+    fetch('./data/wowDiminishingReturns.json', {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+      .then(response => response.json())
+      .then(json => {        
+        let byClassData = json.reduce(keyByClassReducer, {});
+
+        let newState = {
+          spellDataByClass : byClassData
+        };
+        
+        setState(newState);
+      });
+  };
+
+  const renderClassDrList = (className, spellList) => {
+    return (
+      <>
+        <h4>{className}</h4>
+        
+        <table>
+          <thead>
+            <tr>
+              <th>Spell</th>
+              <th>DR School</th>
+              <th>Notes</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            spellList.map(spellData => {
+              return (     
+                <tr>
+                  <td>{spellData.spell}</td>
+                  <td>{spellData.drSchool}</td>
+                  <td>{spellData.note}</td>
+                </tr>
+              );
+            })
+          }
+          </tbody>
+        </table>
+      </>
+    )
+  };
+
+  useEffect(() => {
+    fetchJsonDataAndReduce();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h3>DR by Class:</h3>
+
+      { 
+        state.spellDataByClass && Object.entries(state.spellDataByClass).length > 0 &&
+        Object.entries(state.spellDataByClass).map(([className, spellList]) => {
+          return renderClassDrList(className, spellList);
+        })
+      }
+
     </div>
   );
 }
